b95f0e3d3c691dfa492c69bd48e53091b8e69dd7
Ahsanul Haque <ahsanul0185@gmail.com>
2025-10-07 00:20:37 +0600

    project image added

--- src/components/WorkSlides.jsx
+++ src/components/WorkSlides.jsx
@@ -1,17 +1,13 @@
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import project1 from "../assets/projects/project-1.webp"
-import project2 from "../assets/projects/project-2.webp"
-import project3 from "../assets/projects/project-3.webp"
-import project4 from "../assets/projects/project-4.webp"
-import project5 from "../assets/projects/project-5.webp"
-import project6 from "../assets/projects/project-6.webp"
-import project7 from "../assets/projects/project-7.webp"
-import project8 from "../assets/projects/project-8.webp"
-import project9 from "../assets/projects/project-9.webp"
-import project11 from "../assets/projects/project-11.png"
-import project12 from "../assets/projects/project-12.webp"
+import project1 from "../assets/projects/work-1.png"
+import project2 from "../assets/projects/work-2.png"
+import project3 from "../assets/projects/work-3.jpg"
+import project4 from "../assets/projects/work-4.png"
+import project5 from "../assets/projects/work-5.png"
+import project6 from "../assets/projects/work-6.png"
+
 
 // Import Swiper styles
 import 'swiper/css';
@@ -42,22 +38,7 @@ const works = [
   },
   {
     image : project6
-  },
-  {
-    image : project7
-  },
-  {
-    image : project8
-  },
-  {
-    image : project9
-  },
-  {
-    image : project11
-  },
-  {
-    image : project12
-  },
+  }
 ]
 
 
